fef9246a0293e72bf2a9b2d3627ec1c650acbb45
tanmay_vashistha <tanmayvashistha.mail@gmail.com>
2020-08-01 23:15:05 +0530

    Loader Example with Sign in page Open folder _Loader_

---
+++ Sign In Page/Javascript.js
@@ -0,0 +1,17 @@
+/*no Javascript is required as of now
+function fnc() {
+    myVar = setTimeout(showPage, 3000);
+  }
+  function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("main").style.display = "block";
+  }
+
+  /*function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+  }
+  
+  /*loader function*/
+  /*setTimeout(function(){
+    window.location.href = '../sign-in.html';
+ }, 5000); */   
